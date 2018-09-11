1b733affd5cbfb5ee4b767519d809edee9dafe91
phatblinkie <phatblinkie@hotmail.com>
2018-09-11 04:46:07 +0000

    initial commit

---
+++ node/roller.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/usr/local/bin/geth-roller.0.1.1 --cache=1024 --etherbase="0xe38a257d34a90b497286827ea4d43485f731443b" --maxpeers 100 --extradata "cryptopools.info" --identity "CryptoPools.info - Phatblinkie@hotmail.com for info" --rpc --rpcport 9545 --port 40404 --rpcaddr 127.0.0.1 --rpcapi eth,net,web3 --etherbase="0xe38a257d34a90b497286827ea4d43485f731443b" --extradata "Mined by CryptoPools.info"
+
---
+++ www/config/environment.js
@@ -0,0 +1,70 @@
+/* jshint node: true */
+
+module.exports = function(environment) {
+  var ENV = {
+    modulePrefix: 'open-social-pool',
+    environment: environment,
+    rootURL: '/',
+    locationType: 'hash',
+    EmberENV: {
+      FEATURES: {
+        // Here you can enable experimental features on an ember canary build
+        // e.g. 'with-controller': true
+      }
+    },
+
+    APP: {
+      // API host and port
+      ApiUrl: '//roller.cryptopools.info/',
+      PoolName: 'ROLLER Pool',
+      CompanyName: 'CryptoPools.info',
+      // HTTP mining endpoint
+      HttpHost: 'https://roller.cryptopools.info',
+      HttpPort: 9882,
+
+      // Stratum mining endpoint
+      StratumHost: 'roller.cryptopools.info',
+      StratumPort: 9002,
+
+      // Fee and payout details
+      PoolFee: '1.0%',
+      PayoutThreshold: '1.0',
+      PayoutInterval: '3h',
+
+      // For network hashrate (change for your favourite fork)
+      BlockTime: 15.0,
+      BlockReward: 9,
+      Unit: 'ROL',
+
+    }
+  };
+
+  if (environment === 'development') {
+    /* Override ApiUrl just for development, while you are customizing
+      frontend markup and css theme on your workstation.
+    */
+    ENV.APP.ApiUrl = 'http://localhost:8080/'
+    // ENV.APP.LOG_RESOLVER = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
+
+  if (environment === 'test') {
+    // Testem prefers this...
+    ENV.locationType = 'none';
+
+    // keep test console output quieter
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+    ENV.APP.rootElement = '#ember-testing';
+  }
+
+  if (environment === 'production') {
+
+  }
+
+  return ENV;
+};
